21d6be46482b09c26e119d857e95c8ac779b70c2
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2025-06-07 00:05:48 +0200

    Fix DateAdapter provider error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
+    provideNativeDateAdapter(),
     importProvidersFrom(
       MatButtonModule,
       MatIconModule,
