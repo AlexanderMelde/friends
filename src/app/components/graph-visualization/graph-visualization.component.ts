423ad6a8803860637b9a1abaf48971547741782b
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2025-06-08 22:29:21 +0200

    Updated graph-visualization.component.ts

--- src/app/components/graph-visualization/graph-visualization.component.ts
+++ src/app/components/graph-visualization/graph-visualization.component.ts
@@ -128,7 +128,7 @@ export class GraphVisualizationComponent implements OnInit, AfterViewInit, OnDes
   }
 
   isMobileView(): boolean {
-    return window.innerWidth <= 800;
+    return document.getElementsByClassName('graph-container')[0].innerWidth <= 800;
   }
 
   onTabChange(index: number): void {
