bdbe3c890cc37d78cc334c4938a9a2af91254af1
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2025-06-29 21:19:38 +0200

    Fix missing SectionHeaderComponent import in HelpDialogComponent

--- src/app/components/help-dialog/help-dialog.component.ts
+++ src/app/components/help-dialog/help-dialog.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { SectionHeaderComponent } from '../section-header/section-header.component';
 
 @Component({
   selector: 'app-help-dialog',
@@ -15,7 +16,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    MatExpansionModule
+    MatExpansionModule,
+    SectionHeaderComponent
   ],
   templateUrl: './help-dialog.component.html',
   styleUrls: ['./help-dialog.component.css'],
