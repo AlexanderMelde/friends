6503bff34a742e99e3456a12b6ee3584020e3349
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2025-06-29 02:00:17 +0200

    Fix Angular injection context error for effect()

--- src/app/components/mobile-dialog/mobile-dialog.component.ts
+++ src/app/components/mobile-dialog/mobile-dialog.component.ts
@@ -193,12 +193,15 @@ export class MobileDialogComponent implements OnInit, OnDestroy, AfterViewInit {
       // Set up reactive monitoring of child component's header actions
       if (this.componentRef.instance && this.componentRef.instance.headerActions) {
         // Create an effect to watch for changes in the child component's header actions
-        effect(() => {
-          if (this.componentRef?.instance?.headerActions) {
-            const childActions = this.componentRef.instance.headerActions();
-            this._childHeaderActions.set(childActions);
-          }
-        }, { allowSignalWrites: true });
+        // Wrap the effect call in runInInjectionContext to provide proper injection context
+        this.injector.runInInjectionContext(() => {
+          effect(() => {
+            if (this.componentRef?.instance?.headerActions) {
+              const childActions = this.componentRef.instance.headerActions();
+              this._childHeaderActions.set(childActions);
+            }
+          }, { allowSignalWrites: true });
+        });
       }
 
       // Trigger change detection
